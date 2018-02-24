390f216252cad4206ba59b843048223ea5c6c313
Mike Thompson <mithomps@redhat.com>
2018-02-23 17:06:46 -0800

    Tweaking the cytoscape styles to look better.

--- src/pages/ServiceGraph/CytoscapeConfig.ts
+++ src/pages/ServiceGraph/CytoscapeConfig.ts
@@ -3,44 +3,48 @@ export class CytoscapeConfig {
     return [
       {
         selector: 'node',
-        style: {
-          width: 300,
-          height: 300,
-          content: 'data(label)',
-          //          'text-opacity': 0.5,
-          'text-valign': 'center',
+        css: {
+          content: function(ele: any) {
+            return ele.data('text') || ele.data('id');
+          },
           color: 'white',
           'background-color': '#bbb',
-          'text-outline-width': 2,
-          'text-outline-color': '#999'
+          'text-outline-width': 1,
+          'text-outline-color': '#999',
+          'text-valign': 'center',
+          'text-halign': 'center'
         }
       },
-
+      {
+        selector: '$node > node',
+        css: {
+          'padding-top': '10px',
+          'padding-left': '10px',
+          'padding-bottom': '10px',
+          'padding-right': '10px',
+          color: '#070dff',
+          'text-valign': 'top',
+          'text-halign': 'center',
+          'background-color': '#f5f1d8'
+        }
+      },
       {
         selector: 'edge',
-        style: {
-          width: 10,
-          content: 'data(label)',
+        css: {
+          width: 2,
+          content: 'data(text)',
           'target-arrow-shape': 'triangle',
           'line-color': 'data(color)',
-          'target-arrow-color': '#9dbaea'
+          'target-arrow-color': 'black'
         }
       },
-
       {
         selector: ':selected',
-        style: {
+        css: {
           'background-color': 'yellow',
           'line-color': 'yellow',
-          'target-arrow-color': 'black',
-          'source-arrow-color': 'black'
-        }
-      },
-
-      {
-        selector: 'edge:selected',
-        style: {
-          width: 20
+          'target-arrow-color': 'yellow',
+          'source-arrow-color': 'yellow'
         }
       }
     ];
--- src/pages/ServiceGraph/FakeData.ts
+++ src/pages/ServiceGraph/FakeData.ts
@@ -10,7 +10,7 @@ export class FakeData {
             id: 'n0',
             service: 'productpage.istio-system.svc.cluster.local',
             version: 'v1',
-            label: 'productpage (v1)',
+            text: 'productpage (v1)',
             link_prom_graph:
               'http://prometheus:9090/graph?g0.range_input=1h&g0.tab=0&g0.expr=istio_request_count%7Bdestination_service%3D%22productpage.istio-system.svc.cluster.local%22%2Cdestination_version%3D%22v1%22%7D'
           }
@@ -20,7 +20,7 @@ export class FakeData {
             id: 'n1',
             service: 'reviews.istio-system.svc.cluster.local',
             version: 'v2',
-            label: 'reviews (v2)',
+            text: 'reviews (v2)',
             parent: 'n6',
             link_prom_graph:
               'http://prometheus:9090/graph?g0.range_input=1h&g0.tab=0&g0.expr=istio_request_count%7Bdestination_service%3D%22reviews.istio-system.svc.cluster.local%22%2Cdestination_version%3D%22v2%22%7D'
@@ -31,7 +31,7 @@ export class FakeData {
             id: 'n2',
             service: 'ratings.istio-system.svc.cluster.local',
             version: 'v1',
-            label: 'ratings (v1)',
+            text: 'ratings (v1)',
             link_prom_graph:
               'http://prometheus:9090/graph?g0.range_input=1h&g0.tab=0&g0.expr=istio_request_count%7Bdestination_service%3D%22ratings.istio-system.svc.cluster.local%22%2Cdestination_version%3D%22v1%22%7D'
           }
@@ -41,7 +41,7 @@ export class FakeData {
             id: 'n3',
             service: 'reviews.istio-system.svc.cluster.local',
             version: 'v3',
-            label: 'reviews (v3)',
+            text: 'reviews (v3)',
             parent: 'n6',
             link_prom_graph:
               'http://prometheus:9090/graph?g0.range_input=1h&g0.tab=0&g0.expr=istio_request_count%7Bdestination_service%3D%22reviews.istio-system.svc.cluster.local%22%2Cdestination_version%3D%22v3%22%7D'
@@ -52,7 +52,7 @@ export class FakeData {
             id: 'n4',
             service: 'details.istio-system.svc.cluster.local',
             version: 'v1',
-            label: 'details (v1)',
+            text: 'details (v1)',
             link_prom_graph:
               'http://prometheus:9090/graph?g0.range_input=1h&g0.tab=0&g0.expr=istio_request_count%7Bdestination_service%3D%22details.istio-system.svc.cluster.local%22%2Cdestination_version%3D%22v1%22%7D'
           }
@@ -62,7 +62,7 @@ export class FakeData {
             id: 'n5',
             service: 'reviews.istio-system.svc.cluster.local',
             version: 'v1',
-            label: 'reviews (v1)',
+            text: 'reviews (v1)',
             parent: 'n6',
             link_prom_graph:
               'http://prometheus:9090/graph?g0.range_input=1h&g0.tab=0&g0.expr=istio_request_count%7Bdestination_service%3D%22reviews.istio-system.svc.cluster.local%22%2Cdestination_version%3D%22v1%22%7D'
@@ -72,7 +72,7 @@ export class FakeData {
           data: {
             id: 'n6',
             service: 'reviews.istio-system.svc.cluster.local',
-            label: 'reviews',
+            text: 'reviews',
             color: '#bbb'
           }
         }
@@ -83,7 +83,7 @@ export class FakeData {
             id: 'e0',
             source: 'n0',
             target: 'n1',
-            label: 'rpm=0',
+            text: 'rpm=0',
             color: 'black'
           }
         },
@@ -92,7 +92,7 @@ export class FakeData {
             id: 'e1',
             source: 'n1',
             target: 'n2',
-            label: 'rpm=0.7',
+            text: 'rpm=0.7',
             color: 'orange'
           }
         },
@@ -101,7 +101,7 @@ export class FakeData {
             id: 'e2',
             source: 'n0',
             target: 'n3',
-            label: 'rpm=0',
+            text: 'rpm=0',
             color: 'black'
           }
         },
@@ -110,7 +110,7 @@ export class FakeData {
             id: 'e3',
             source: 'n3',
             target: 'n2',
-            label: 'rpm=0',
+            text: 'rpm=0',
             color: 'black'
           }
         },
@@ -119,7 +119,7 @@ export class FakeData {
             id: 'e4',
             source: 'n0',
             target: 'n4',
-            label: 'rpm=0',
+            text: 'rpm=0',
             color: 'black'
           }
         },
@@ -128,7 +128,7 @@ export class FakeData {
             id: 'e5',
             source: 'n0',
             target: 'n5',
-            label: 'rpm=0',
+            text: 'rpm=0',
             color: 'black'
           }
         }
