8dc63c0f954eead1eadba6edf19e4220fb0010f3
Alberto Gutierrez <aljesusg@gmail.com>
2018-03-22 18:21:08 +0100

    Change to backend

--- src/pages/ServiceDetails/ServiceDetailsPage.tsx
+++ src/pages/ServiceDetails/ServiceDetailsPage.tsx
@@ -6,57 +6,77 @@ import ServiceId from '../../types/ServiceId';
 import { Nav, NavItem, TabContainer, TabContent, TabPane } from 'patternfly-react';
 import { NamespaceFilterSelected } from '../../components/NamespaceFilter/NamespaceFilter';
 import { ActiveFilter } from '../../types/NamespaceFilter';
+import * as API from '../../services/Api';
 
-const ServiceDetails = (routeProps: RouteComponentProps<ServiceId>) => {
-  let updateFilter = () => {
+type ServiceDetailsState = {
+  jaegerUri: string;
+};
+
+class ServiceDetails extends React.Component<RouteComponentProps<ServiceId>, ServiceDetailsState> {
+  constructor(props: RouteComponentProps<ServiceId>) {
+    super(props);
+    this.state = {
+      jaegerUri: ''
+    };
+  }
+  updateFilter = () => {
     let activeFilter: ActiveFilter = {
-      label: 'Namespace: ' + routeProps.match.params.namespace,
+      label: 'Namespace: ' + this.props.match.params.namespace,
       category: 'Namespace',
-      value: routeProps.match.params.namespace.toString()
+      value: this.props.match.params.namespace.toString()
     };
     NamespaceFilterSelected.setSelected([activeFilter]);
   };
-  const tracesJaeger = `http://jaeger-query-istio-system.127.0.0.1.nip.io/search?service=${
-    routeProps.match.params.service
-  }`;
-  return (
-    <div className="container-fluid container-pf-nav-pf-vertical">
-      <div className="page-header">
-        <h2>
-          Service{' '}
-          <Link to="/services" onClick={updateFilter}>
-            {routeProps.match.params.namespace}
-          </Link>{' '}
-          / {routeProps.match.params.service}
-        </h2>
-      </div>
-      <TabContainer id="basic-tabs" defaultActiveKey={1}>
-        <div>
-          <Nav bsClass="nav nav-tabs nav-tabs-pf">
-            <NavItem eventKey={1}>
-              <div dangerouslySetInnerHTML={{ __html: 'Info' }} />
-            </NavItem>
-            <NavItem eventKey={2}>
-              <div dangerouslySetInnerHTML={{ __html: 'Metrics' }} />
-            </NavItem>
-            <li role="presentation">
-              <a href={tracesJaeger} target="_blank">
-                <div dangerouslySetInnerHTML={{ __html: 'Traces' }} />
-              </a>
-            </li>
-          </Nav>
-          <TabContent>
-            <TabPane eventKey={1}>
-              <ServiceInfo namespace={routeProps.match.params.namespace} service={routeProps.match.params.service} />
-            </TabPane>
-            <TabPane eventKey={2}>
-              <ServiceMetrics namespace={routeProps.match.params.namespace} service={routeProps.match.params.service} />
-            </TabPane>
-          </TabContent>
+  componentWillMount() {
+    API.getJaegerInfo().then(response => {
+      this.setState({
+        jaegerUri: `${response['data'].url}/search?service=${this.props.match.params.service}`
+      });
+    });
+  }
+  render() {
+    return (
+      <div className="container-fluid container-pf-nav-pf-vertical">
+        <div className="page-header">
+          <h2>
+            Service{' '}
+            <Link to="/services" onClick={this.updateFilter}>
+              {this.props.match.params.namespace}
+            </Link>{' '}
+            / {this.props.match.params.service}
+          </h2>
         </div>
-      </TabContainer>
-    </div>
-  );
-};
+        <TabContainer id="basic-tabs" defaultActiveKey={1}>
+          <div>
+            <Nav bsClass="nav nav-tabs nav-tabs-pf">
+              <NavItem eventKey={1}>
+                <div dangerouslySetInnerHTML={{ __html: 'Info' }} />
+              </NavItem>
+              <NavItem eventKey={2}>
+                <div dangerouslySetInnerHTML={{ __html: 'Metrics' }} />
+              </NavItem>
+              <li role="presentation">
+                <a href={this.state.jaegerUri} target="_blank">
+                  <div dangerouslySetInnerHTML={{ __html: 'Traces' }} />
+                </a>
+              </li>
+            </Nav>
+            <TabContent>
+              <TabPane eventKey={1}>
+                <ServiceInfo namespace={this.props.match.params.namespace} service={this.props.match.params.service} />
+              </TabPane>
+              <TabPane eventKey={2}>
+                <ServiceMetrics
+                  namespace={this.props.match.params.namespace}
+                  service={this.props.match.params.service}
+                />
+              </TabPane>
+            </TabContent>
+          </div>
+        </TabContainer>
+      </div>
+    );
+  }
+}
 
 export default ServiceDetails;
