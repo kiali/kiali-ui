51a14e9bf81118d0ed30d12052bad3fb88f34df4
Edgar Hernández <ehernand@redhat.com>
2018-04-19 19:04:12 -0500

    KIALI-592: Show circuit breakers when graph node is selected

--- src/pages/ServiceGraph/SummaryPanelNode.tsx
+++ src/pages/ServiceGraph/SummaryPanelNode.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as API from '../../services/Api';
+import * as pf from 'patternfly-react';
 
 import graphUtils from '../../utils/graphing';
 import { getTrafficRate, getAccumulatedTrafficRate } from '../../utils/TrafficRate';
@@ -8,6 +9,8 @@ import InOutRateTable from '../../components/SummaryPanel/InOutRateTable';
 import RpsChart from '../../components/SummaryPanel/RpsChart';
 import { SummaryPanelPropType } from '../../types/Graph';
 import MetricsOptions from '../../types/MetricsOptions';
+import LocalTime from '../../components/Time/LocalTime';
+import RouteRuleIstioService from '../ServiceDetails/ServiceInfo/ServiceInfoRouteRules/RouteRuleIstioService';
 
 type SummaryPanelStateType = {
   loading: boolean;
@@ -135,18 +138,31 @@ export default class SummaryPanelNode extends React.Component<SummaryPanelPropTy
           </div>
         </div>
         <div className="panel-body">
-          <InOutRateTable
-            title="Request Traffic (requests per second):"
-            inRate={incoming.rate}
-            inRate3xx={incoming.rate3xx}
-            inRate4xx={incoming.rate4xx}
-            inRate5xx={incoming.rate5xx}
-            outRate={outgoing.rate}
-            outRate3xx={outgoing.rate3xx}
-            outRate4xx={outgoing.rate4xx}
-            outRate5xx={outgoing.rate5xx}
-          />
-          <div>{this.renderRpsCharts()}</div>
+          <pf.TabContainer defaultActiveKey="traffic">
+            <div>
+              <pf.Nav bsClass="nav nav-tabs">
+                <pf.NavItem eventKey="traffic">Traffic</pf.NavItem>
+                <pf.NavItem eventKey="ci">Circuit Breakers</pf.NavItem>
+              </pf.Nav>
+              <pf.TabContent>
+                <pf.TabPane eventKey="traffic">
+                  <InOutRateTable
+                    title="Request Traffic (requests per second):"
+                    inRate={incoming.rate}
+                    inRate3xx={incoming.rate3xx}
+                    inRate4xx={incoming.rate4xx}
+                    inRate5xx={incoming.rate5xx}
+                    outRate={outgoing.rate}
+                    outRate3xx={outgoing.rate3xx}
+                    outRate4xx={outgoing.rate4xx}
+                    outRate5xx={outgoing.rate5xx}
+                  />
+                  <div>{this.renderRpsCharts()}</div>
+                </pf.TabPane>
+                <pf.TabPane eventKey="ci">{this.renderCircuitBreakers()}</pf.TabPane>
+              </pf.TabContent>
+            </div>
+          </pf.TabContainer>
         </div>
       </div>
     );
@@ -170,5 +186,81 @@ export default class SummaryPanelNode extends React.Component<SummaryPanelPropTy
         />
       </>
     );
+  };
+
+  private renderCircuitBreakers = () => {
+    const dps = this.props.data.summaryTarget.data('destinationPolicies');
+    if (!dps) {
+      return null;
+    }
+
+    return dps.map((dPolicy, i) => {
+      let circuitBreaker;
+      if (dPolicy.circuitBreaker) {
+        circuitBreaker = (
+          <div>
+            <strong>CircuitBreaker</strong>
+            <ul style={{ listStyleType: 'none' }}>
+              {dPolicy.circuitBreaker.simpleCb ? (
+                <li>
+                  <strong>simpleCb</strong>
+                  <ul style={{ listStyleType: 'none' }}>
+                    {dPolicy.circuitBreaker.simpleCb.maxConnections ? (
+                      <li>[maxConnections] {dPolicy.circuitBreaker.simpleCb.maxConnections}</li>
+                    ) : null}
+                    {dPolicy.circuitBreaker.simpleCb.httpMaxPendingRequests ? (
+                      <li>[httpMaxPendingRequests] {dPolicy.circuitBreaker.simpleCb.httpMaxPendingRequests}</li>
+                    ) : null}
+                    {dPolicy.circuitBreaker.simpleCb.httpMaxRequests ? (
+                      <li>[httpMaxRequests] {dPolicy.circuitBreaker.simpleCb.httpMaxRequests}</li>
+                    ) : null}
+                    {dPolicy.circuitBreaker.simpleCb.sleepWindow ? (
+                      <li>[sleepWindow] {dPolicy.circuitBreaker.simpleCb.sleepWindow}</li>
+                    ) : null}
+                    {dPolicy.circuitBreaker.simpleCb.httpConsecutiveErrors ? (
+                      <li>[httpConsecutiveErrors] {dPolicy.circuitBreaker.simpleCb.httpConsecutiveErrors}</li>
+                    ) : null}
+                    {dPolicy.circuitBreaker.simpleCb.httpDetectionInterval ? (
+                      <li>[httpDetectionInterval] {dPolicy.circuitBreaker.simpleCb.httpDetectionInterval}</li>
+                    ) : null}
+                    {dPolicy.circuitBreaker.simpleCb.httpMaxRequestsPerConnection ? (
+                      <li>
+                        [httpMaxRequestsPerConnection] {dPolicy.circuitBreaker.simpleCb.httpMaxRequestsPerConnection}
+                      </li>
+                    ) : null}
+                    {dPolicy.circuitBreaker.simpleCb.httpMaxEjectionPercent ? (
+                      <li>[httpMaxEjectionPercent] {dPolicy.circuitBreaker.simpleCb.httpMaxEjectionPercent}</li>
+                    ) : null}
+                    {dPolicy.circuitBreaker.simpleCb.httpMaxRetries ? (
+                      <li>[httpMaxRetries] {dPolicy.circuitBreaker.simpleCb.httpMaxRetries}</li>
+                    ) : null}
+                  </ul>
+                </li>
+              ) : null}
+              {dPolicy.circuitBreaker.custom ? <li>[custom] {dPolicy.circuitBreaker.custom}</li> : null}
+            </ul>
+          </div>
+        );
+      }
+      return (
+        <div key={'rule' + i}>
+          <div>
+            <strong>Name</strong>
+            {': '}
+            {dPolicy.name}
+          </div>
+          <div>
+            <strong>Created at</strong>
+            {': '}
+            <LocalTime time={dPolicy.created_at} />
+          </div>
+          {dPolicy.destination ? <RouteRuleIstioService name="Destination" service={dPolicy.destination} /> : null}
+          {dPolicy.source ? <RouteRuleIstioService name="Source" service={dPolicy.source} /> : null}
+
+          {circuitBreaker}
+          <hr />
+        </div>
+      );
+    });
   };
 }
