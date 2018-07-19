e454497b46bcecfb452b6d34af156a503c57ad47
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-07-19 19:08:18 +0200

    KIALI-1173 Keep ServiceDetail tabs tracked

--- src/pages/ServiceDetails/IstioObjectDetails.tsx
+++ src/pages/ServiceDetails/IstioObjectDetails.tsx
@@ -139,7 +139,7 @@ export default class IstioObjectDetails extends React.Component<IstioObjectDetai
             <TabContainer
               id="basic-tabs"
               activeKey={this.props.activeTab('detail', 'overview')}
-              onSelect={this.istioDetailTabSelectHandler}
+              onSelect={this.props.onSelectTab('detail')}
             >
               <>
                 {this.renderTabNav()}
@@ -154,8 +154,4 @@ export default class IstioObjectDetails extends React.Component<IstioObjectDetai
       </div>
     );
   }
-
-  private istioDetailTabSelectHandler = (tabKey?: string) => {
-    this.props.onSelectTab('detail', tabKey);
-  };
 }
