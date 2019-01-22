c9f26ba0e60e315d19b4c97724845a24aa47d801
Alberto Gutierrez <aljesusg@gmail.com>
2019-01-22 11:14:33 +0100

    KIALI-2088 Add pf4 style and remove font body in index.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app/App';
 import './index.css';
+import '@patternfly/react-core/dist/styles/base.css';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
