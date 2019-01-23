196035185f67cf0a220859c81ccc5932cc542b79
Josejulio Martínez <jmartine@redhat.com>
2019-01-23 11:46:28 -0600

    Triggers a warning if copied text changed or an error if failed to copy

--- src/components/DebugInformation/DebugInformation.tsx
+++ src/components/DebugInformation/DebugInformation.tsx
@@ -7,13 +7,20 @@ import { style } from 'typestyle';
 import memoizeOne from 'memoize-one';
 import beautify from 'json-beautify';
 
+enum CopyStatus {
+  NOT_COPIED, // We haven't copied the current output
+  COPIED, // Current output is in the clipboard
+  OLD_COPY, // We copied the prev output, but there are changes in the KialiAppState
+  FAILED_COPY // For some reason we failed to copy and we need to suggest to use ctrl+c
+}
+
 type DebugInformationProps = {
   appState: KialiAppState;
 };
 
 type DebugInformationState = {
   show: boolean;
-  copied: boolean;
+  copyStatus: CopyStatus;
 };
 
 const textAreaStyle = style({
@@ -32,11 +39,11 @@ export default class DebugInformation extends React.PureComponent<DebugInformati
   constructor(props: DebugInformationProps) {
     super(props);
     this.textareaRef = React.createRef();
-    this.state = { show: false, copied: false };
+    this.state = { show: false, copyStatus: CopyStatus.NOT_COPIED };
   }
 
   open = () => {
-    this.setState({ show: true, copied: false });
+    this.setState({ show: true, copyStatus: CopyStatus.NOT_COPIED });
   };
 
   close = () => {
@@ -45,15 +52,22 @@ export default class DebugInformation extends React.PureComponent<DebugInformati
 
   copyCallback = (text: string, result: boolean) => {
     this.textareaRef.current.select();
-    this.setState({ copied: result });
+    this.setState({ copyStatus: result ? CopyStatus.COPIED : CopyStatus.FAILED_COPY });
   };
 
-  hideCopySuccess = () => {
-    this.setState({ copied: false });
+  hideAlert = () => {
+    this.setState({ copyStatus: CopyStatus.NOT_COPIED });
   };
+
+  componentDidUpdate(prevProps: DebugInformationProps, prevState: DebugInformationState) {
+    if (this.props.appState !== prevProps.appState && this.state.copyStatus === CopyStatus.COPIED) {
+      this.setState({ copyStatus: CopyStatus.OLD_COPY });
+    }
+  }
 
   render() {
     const renderDebugInformation = memoizeOne(() => {
+      console.log('DEBUG: I got called');
       const debugInformation: DebugInformationData = {
         currentURL: window.location.href,
         reduxState: this.props.appState
@@ -83,9 +97,20 @@ export default class DebugInformation extends React.PureComponent<DebugInformati
           <Modal.Title>Debug information</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {this.state.copied && (
-            <Alert type="success" onDismiss={this.hideCopySuccess}>
+          {this.state.copyStatus === CopyStatus.COPIED && (
+            <Alert type="success" onDismiss={this.hideAlert}>
               Debug information has been copied to your clipboard.
+            </Alert>
+          )}
+          {this.state.copyStatus === CopyStatus.OLD_COPY && (
+            <Alert type="warning" onDismiss={this.hideAlert}>
+              Debug information was copied to your clipboard, but is outdated now. It could be caused by new data
+              received by auto refresh timers.
+            </Alert>
+          )}
+          {this.state.copyStatus === CopyStatus.FAILED_COPY && (
+            <Alert type="error" onDismiss={this.hideAlert}>
+              We failed to automatically copy the text, please use ctrl + c.
             </Alert>
           )}
           <span>Please include this information when opening a bug.</span>
