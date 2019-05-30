a6c34f7472479e70a3059cfeb23ceac9de8cfc44
Alberto Gutierrez <aljesusg@gmail.com>
2019-05-30 11:30:49 +0200

    KIALI-2911 Add options to menu in graph

--- src/components/CytoscapeGraph/ContextMenu/NodeContextMenu.tsx
+++ src/components/CytoscapeGraph/ContextMenu/NodeContextMenu.tsx
@@ -1,29 +1,63 @@
 import * as React from 'react';
 import { NodeContextMenuProps } from '../CytoscapeContextMenu';
+import { JaegerSearchOptions, JaegerURLSearch } from '../../JaegerIntegration/RouteHelper';
 import history from '../../../app/History';
+import { Paths } from '../../../config';
 
-export class NodeContextMenu extends React.PureComponent<NodeContextMenuProps> {
-  // @todo: We need take care of this at global app level
-  private static makeDetailsPageUrl(props: NodeContextMenuProps) {
-    const namespace = props.namespace;
-    const nodeType = props.nodeType;
-    const workload = props.workload;
-    let app = props.app;
-    let urlNodeType = app;
-    if (nodeType === 'app') {
-      urlNodeType = 'applications';
-    } else if (nodeType === 'service') {
-      urlNodeType = 'services';
-    } else if (workload) {
-      urlNodeType = 'workloads';
-      app = workload;
+type NodeContextMenuState = {
+  nodeType: string;
+  app: string | undefined;
+};
+
+export class NodeContextMenu extends React.PureComponent<NodeContextMenuProps, NodeContextMenuState> {
+  constructor(props: NodeContextMenuProps) {
+    super(props);
+    let app = this.props.app;
+    let nodeType = '';
+    switch (this.props.nodeType) {
+      case 'app':
+        nodeType = Paths.APPLICATIONS;
+        break;
+      case 'service':
+        nodeType = Paths.SERVICES;
+        break;
+      case 'workload':
+        app = this.props.workload;
+        nodeType = Paths.WORKLOADS;
+        break;
+      default:
     }
-    return `/namespaces/${namespace}/${urlNodeType}/${app}`;
+    this.state = { nodeType, app };
+  }
+  // @todo: We need take care of this at global app level
+  makeDetailsPageUrl() {
+    return `/namespaces/${this.props.namespace}/${this.state.nodeType}/${this.state.app}`;
+  }
+
+  getJaegerURL() {
+    let tracesUrl = `/jaeger?namespaces=${this.props.namespace}&service=${this.state.app}.${this.props.namespace}`;
+    if (!this.props.jaegerIntegration) {
+      const url = new JaegerURLSearch(this.props.jaegerURL, false);
+      const options: JaegerSearchOptions = {
+        serviceSelected: `${this.state.app}.${this.props.namespace}`,
+        limit: 20,
+        start: '',
+        end: '',
+        minDuration: '',
+        maxDuration: '',
+        lookback: '3600',
+        tags: ''
+      };
+
+      tracesUrl = url.createRoute(options);
+    }
+    return tracesUrl;
   }
 
   render() {
     const version = this.props.version ? `${this.props.version}` : '';
-    const detailsPageUrl = NodeContextMenu.makeDetailsPageUrl(this.props);
+    const detailsPageUrl = this.makeDetailsPageUrl();
+    const nodetype = this.state.nodeType;
     return (
       <div className="kiali-graph-context-menu-container">
         <div className="kiali-graph-context-menu-title">
@@ -34,16 +68,69 @@ export class NodeContextMenu extends React.PureComponent<NodeContextMenuProps> {
             Show Details
           </a>
         </div>
+        <div className="kiali-graph-context-menu-item">
+          <a
+            onClick={this.redirectContextLink}
+            className="kiali-graph-context-menu-item-link"
+            href={`${detailsPageUrl}?tab=traffic`}
+          >
+            Show Traffic
+          </a>
+        </div>
+        {nodetype === Paths.WORKLOADS && (
+          <div className="kiali-graph-context-menu-item">
+            <a
+              onClick={this.redirectContextLink}
+              className="kiali-graph-context-menu-item-link"
+              href={`${detailsPageUrl}?tab=logs`}
+            >
+              Show Logs
+            </a>
+          </div>
+        )}
+        <div className="kiali-graph-context-menu-item">
+          <a
+            onClick={this.redirectContextLink}
+            className="kiali-graph-context-menu-item-link"
+            href={`${detailsPageUrl}?tab=${nodetype === Paths.SERVICES ? 'metrics' : 'in_metrics'}`}
+          >
+            Show Inbound Metrics
+          </a>
+        </div>
+        {nodetype !== Paths.SERVICES && (
+          <div className="kiali-graph-context-menu-item">
+            <a
+              onClick={this.redirectContextLink}
+              className="kiali-graph-context-menu-item-link"
+              href={`${detailsPageUrl}?tab=out_metrics`}
+            >
+              Show Outbound Metrics
+            </a>
+          </div>
+        )}
+        {nodetype === Paths.SERVICES && this.props.jaegerURL !== '' && (
+          <div className="kiali-graph-context-menu-item">
+            <a
+              onClick={this.redirectContextLink}
+              className="kiali-graph-context-menu-item-link"
+              target={this.props.jaegerIntegration ? '_self' : '_blank'}
+              href={this.getJaegerURL()}
+            >
+              Show Traces
+            </a>
+          </div>
+        )}
       </div>
     );
   }
 
   private redirectContextLink = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
     if (e.target) {
       const anchor = e.target as HTMLAnchorElement;
       const href = anchor.getAttribute('href');
-      if (href) {
+      const newTab = anchor.getAttribute('target') === '_blank';
+      if (href && !newTab) {
+        e.preventDefault();
         this.props.contextMenu.hide(0);
         history.push(href);
       }
