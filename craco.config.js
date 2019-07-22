7694b38cc376a41553b2d8a9a69a2e6d855604e0
Jean-Christophe Counio <jean-christophe.counio@wework.com>
2019-07-21 20:00:38 -0700

    Fix comment in js  file

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
-# This a workaround to avoid to be annoyed with webcomponent intergration during testing. "etag" is just a random package that doesn't interfere with the rest of the tests
+// This a workaround to avoid to be annoyed with webcomponent intergration during testing. 
+// "etag" is just a random package that doesn't interfere with the rest of the tests
 module.exports = {
     jest: {
         configure: {
