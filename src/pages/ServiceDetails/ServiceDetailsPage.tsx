5adb291cc0ef0383964b5d10294685b4bb93b4f2
Alberto Gutierrez <aljesusg@gmail.com>
2020-04-22 14:28:35 +0200

    Merge and add comments

--- src/pages/ServiceDetails/ServiceDetailsPage.tsx
+++ src/pages/ServiceDetails/ServiceDetailsPage.tsx
@@ -31,7 +31,7 @@ import { DurationDropdownContainer } from '../../components/DurationDropdown/Dur
 import RefreshButtonContainer from '../../components/Refresh/RefreshButton';
 import IstioWizardDropdown from '../../components/IstioWizards/IstioWizardDropdown';
 import { JaegerErrors, JaegerTrace, JaegerInfo } from '../../types/JaegerInfo';
-import { getQueryJaeger } from '../../components/JaegerIntegration/RouteHelper';
+import { changeParams, getQueryJaeger } from '../../components/JaegerIntegration/RouteHelper';
 import RefreshContainer from '../../components/Refresh/Refresh';
 import { PfColors } from '../../components/Pf/PfColors';
 import TimeRangeComponent from 'components/Time/TimeRangeComponent';
@@ -108,6 +108,7 @@ const tabIndex: { [tab: string]: number } = {
 class ServiceDetails extends React.Component<ServiceDetailsProps, ServiceDetailsState> {
   private promises = new PromisesRegistry();
   private traces: JaegerTrace[] = [];
+  private traceParams: any = {};
   private lastFetchTraceMicros: number | undefined = undefined;
   private lastFetchTracesError = false;
   private graphDataSource: GraphDataSource;
@@ -322,19 +323,25 @@ class ServiceDetails extends React.Component<ServiceDetailsProps, ServiceDetails
           }
         });
     } else {
+      // Get actual Params
       const params = getQueryJaeger();
       const fetchParams = { ...params }; // copy
-      if (this.lastFetchTraceMicros) {
+      // If user change tha params like tags or limit so we need to reset lastFetchTraceMicros
+      if (changeParams(this.traceParams)) {
+        this.lastFetchTraceMicros = undefined;
+      } else {
+        // If not we need to set out fetch to the lastFetchTraceMicros
         fetchParams[URLParam.JAEGER_START_TIME] = this.lastFetchTraceMicros;
       }
-      fetchTraces(this.props.match.params.namespace, this.props.match.params.service, params)
+      fetchTraces(this.props.match.params.namespace, this.props.match.params.service, fetchParams)
         .then(traces => {
           const appendTraces = this.lastFetchTraceMicros !== undefined;
           this.lastFetchTracesError = false;
           let myState = {};
           if (traces && traces.data) {
+            // We get new trace results so we need to concatenate them to the results that we have
             this.traces = appendTraces
-              ? this.traces.filter(t => t.startTime >= params[URLParam.JAEGER_START_TIME]).concat(traces.data)
+              ? this.traces.filter(t => t.startTime >= fetchParams[URLParam.JAEGER_START_TIME]).concat(traces.data)
               : traces.data;
             if (traces.data.length === 0) {
               myState['selectedTrace'] = undefined;
