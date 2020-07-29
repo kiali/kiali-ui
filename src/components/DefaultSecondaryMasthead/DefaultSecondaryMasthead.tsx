025c5a5c18fca991bdf0c54182a28a9fe6744e56
Yew-Huey Liu <yhliu@us.ibm.com>
2020-07-29 15:16:59 -0400

    Fix change request of PR 1841
    1. change the orange switch to dropdown selection in the creat experiment page
    2. disable the generic namespaces dropdown on the top

--- src/components/DefaultSecondaryMasthead/DefaultSecondaryMasthead.tsx
+++ src/components/DefaultSecondaryMasthead/DefaultSecondaryMasthead.tsx
@@ -12,12 +12,14 @@ const titles = [
   'extensions/iter8',
   'extensions/iter8/new'
 ];
+
 export default class DefaultSecondaryMasthead extends React.Component {
   showTitle() {
     let path = window.location.pathname;
     path = path.substr(path.lastIndexOf('/console') + '/console'.length + 1);
     if (titles.includes(path)) {
       let title = path.charAt(0).toUpperCase() + path.slice(1);
+      let disabled = false;
       if (path === 'istio/new') {
         title = 'Create New Istio Config';
       } else if (path === 'istio') {
@@ -26,22 +28,26 @@ export default class DefaultSecondaryMasthead extends React.Component {
         title = 'Iter8 Experiments';
       } else if (path === 'extensions/iter8/new') {
         title = 'Create New Iter8 Experiment';
+        disabled = true;
       }
-      return (
-        <Title headingLevel="h1" size="3xl" style={{ margin: '18px 0 18px' }}>
-          {title}
-        </Title>
-      );
+      return {
+        title: (
+          <Title headingLevel="h1" size="3xl" style={{ margin: '18px 0 18px' }}>
+            {title}
+          </Title>
+        ),
+        disabled: disabled
+      };
     }
 
-    return undefined;
+    return { title: undefined, disabled: false };
   }
 
   render() {
-    const title = this.showTitle();
+    const { title, disabled } = this.showTitle();
     return (
       <SecondaryMasthead title={title ? true : false}>
-        <NamespaceDropdownContainer disabled={false} />
+        <NamespaceDropdownContainer disabled={disabled} />
         {title}
       </SecondaryMasthead>
     );
