f0b29a67700c6fd5c3e273c12c53dd36fa8a1e9a
Alberto Gutierrez <aljesusg@gmail.com>
2020-11-26 12:39:31 +0100

    Replace multiple x

--- src/types/ErrorRate/utils.ts
+++ src/types/ErrorRate/utils.ts
@@ -174,7 +174,7 @@ export const parseAnnotation = (tolerance: string): ToleranceConfig => {
       protocol = new RegExp(cads[0]);
     }
   }
-  const regex = code.toLowerCase().replace('x', '\\d');
+  const regex = code.replace(/x|X/g, '\\d');
   return {
     code: new RegExp(regex),
     degraded: degraded,
