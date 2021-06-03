bb4709a767cf8f7229184b6fdad9ec08b193bd19
Leandro Beretta <lea.beretta@gmail.com>
2021-06-03 16:22:20 -0300

    avoid showing envoy tabs if at least one pod doesn't have the sidecar injected

--- src/pages/WorkloadDetails/WorkloadDetailsPage.tsx
+++ src/pages/WorkloadDetails/WorkloadDetailsPage.tsx
@@ -41,11 +41,7 @@ const paramToTab: { [key: string]: number } = {
   logs: 2,
   in_metrics: 3,
   out_metrics: 4,
-  traces: 5,
-  envoy_listeners: 6,
-  envoy_clusters: 7,
-  envoy_routes: 8,
-  envoy_config: 9
+  traces: 5
 };
 var nextTabIndex = 6;
 
