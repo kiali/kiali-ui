2610212e17e6791138ca70d53ae2b4b6d6c91daa
Nick Fox <nfox@redhat.com>
2022-01-31 16:07:39 -0500

    Move generated json to its own folder

--- cypress/integration/mock-server/mock.spec.ts
+++ cypress/integration/mock-server/mock.spec.ts
@@ -1,6 +1,6 @@
 describe('Mock server', () => {
   it('Runs forever and intercepts graph api calls', () => {
-    cy.intercept('GET', '/kiali/api/namespaces/graph*', { fixture: 'large_graph.json' });
+    cy.intercept('GET', '/api/namespaces/graph*', { fixture: 'generated/generated_graph_data.json' });
     cy.visit(Cypress.env().KIALI_URL);
     cy.pause();
   });
