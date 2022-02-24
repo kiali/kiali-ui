054e2736672966cbf94ea730f8064e40e8645ed7
ScriptingShrimp <scriptingshrimp@protonmail.com>
2022-02-25 00:48:10 +0100

    bdd init commit
    fixing node types version
    
    fixing feature files - first tests
    
    yarn lock

---
+++ cypress/integration/common/kiali_homepage.ts
@@ -0,0 +1,13 @@
+import { Given } from "cypress-cucumber-preprocessor/steps";
+import { Then } from "cypress-cucumber-preprocessor/steps";
+
+const url = "https://kiali-istio-system.apps.pmdown4.0216-eg3.fw.rhcloud.com"
+
+Given('I open Kiali URL', () => {
+  cy.visit(url)
+})
+
+
+Then(`I see {string} in the title`, (title) => {
+  cy.title().should('include', title)
+})
