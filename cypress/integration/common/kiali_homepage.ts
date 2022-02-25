cdd916d1300c5099f366e230ad0b720751eb60c8
prachiyadav <pyadav@redhat.com>
2022-02-25 18:22:18 +0530

    kiali login test

--- cypress/integration/common/kiali_homepage.ts
+++ cypress/integration/common/kiali_homepage.ts
@@ -1,9 +1,9 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
+import { And, Given } from "cypress-cucumber-preprocessor/steps";
 import { Then } from "cypress-cucumber-preprocessor/steps";
 
 const url = "https://kiali-istio-system.apps.pmdown4.0216-eg3.fw.rhcloud.com"
 
-Given('I open Google page', () => {
+Given('I open Kiali URL', () => {
   cy.visit(url)
 })
 
@@ -11,3 +11,15 @@ Given('I open Google page', () => {
 Then(`I see {string} in the title`, (title) => {
   cy.title().should('include', title)
 })
+
+
+Then(`I see {string} button`, (title) => {
+  cy.get('button[type=submit]').click()
+
+})
+
+//And(`I click`, (title) => {
+//  cy.get('button[type=submit]').click()
+//})
+
+// <button class="pf-c-button pf-m-primary" type="submit" 
--- cypress/integration/featureFiles/kiali_homepage.feature
+++ cypress/integration/featureFiles/kiali_homepage.feature
@@ -2,7 +2,11 @@ Feature: Kilai login
 
   I want to login to Kiali and see landing page
   
-  @focus
   Scenario: Open Kaili home page
     Given I open Kiali URL
-    Then I see "Google" in the title
+    Then I see "Kiali" in the title
+
+  Scenario: Login intio kiali
+    Given I open Kiali URL
+    Then I see "Log In With OpenShift" button
+    
