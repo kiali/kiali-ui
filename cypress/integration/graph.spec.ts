7d3cb55d8d1e8a848c41f91674ff91d11c9e7aaf
Nick Fox <nfox@redhat.com>
2022-02-24 22:09:34 -0500

    Add large graph fixture and test using it

--- cypress/integration/graph.spec.ts
+++ cypress/integration/graph.spec.ts
@@ -1,3 +1,5 @@
+import { GraphDefinition } from '../../src/types/Graph';
+
 describe('Graph page', () => {
   before(() => {
     // Example:
@@ -12,5 +14,40 @@ describe('Graph page', () => {
 
     cy.contains('Loading Graph');
     cy.get('.graph', { timeout: 5000 });
+  });
+
+  describe('large graphs', () => {
+    const namespaces = new Set<string>();
+    before(() => {
+      cy.fixture('large_graph').then((graphData: GraphDefinition) => {
+        graphData.elements.nodes.forEach(node => {
+          if (node.data.namespace.startsWith('n')) {
+            namespaces.add(node.data.namespace);
+          }
+        });
+        cy.log('Creating namespaces...');
+        namespaces.forEach(ns => {
+          cy.exec(`kubectl get ns ${ns} || kubectl create namespace ${ns}`);
+        });
+
+        cy.intercept('GET', '/api/namespaces/graph*', graphData);
+      });
+    });
+
+    // Cleanup
+    after(() => {
+      cy.log('Deleting namespaces...');
+      namespaces.forEach(ns => {
+        cy.exec(`kubectl delete namespace ${ns} --wait=false || true`);
+      });
+    });
+
+    it('loads a large graph', () => {
+      cy.visit('/console/graph/namespaces/?refresh=0');
+      cy.selectAllNamespaces();
+
+      cy.contains('Loading Graph');
+      cy.get('.graph', { timeout: 60000 });
+    });
   });
 });
